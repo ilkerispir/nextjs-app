6b9c0db65251da46098db2a2a1807c8ff68eec92
ilkerispir <ilker.ispir@alo-tech.com>
2021-05-09 00:43:27 +0300

    init commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import SignInSide from './components/login';
+
+
+export default function Home() {
+  return (
+    <SignInSide />
+  )
+}
